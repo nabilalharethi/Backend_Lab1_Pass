ad05adcca797aa33db68f9c3cca352ea03568a72
Nabil Abdulbari Abdulqader Al-Harethi <na223bj@student.lnu.se>
2025-12-10 00:14:37 +0100

    add route to fetch  products by id

--- src/routes/products.js
+++ src/routes/products.js
@@ -14,3 +14,16 @@ router.post('/', async (req, res) => {
         res.status(500).json({ error: 'Internal Server Error' });
     }
 });
+
+router.get('/:id', async (req, res) => {
+    try {
+        const product = await Product.getById(req.params.id);
+        if (!product) {
+            return  res.status(404).json({ error: 'Product not found' });
+        }
+        res.json(product);
+    } catch (error) {
+        console.error('Error fetching product:', error.message);
+        res.status(400).json({ error: 'Invalid product ID' });
+    }
+});
